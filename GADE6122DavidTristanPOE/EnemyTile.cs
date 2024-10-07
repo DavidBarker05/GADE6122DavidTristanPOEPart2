1454ced7d673b02f5f2664978a7ac9aa7d86e1d2
DavidBarker05 <davidbarker9611@gmail.com>
2024-10-07 15:17:58 +0200

    Fixed EnemyTile class

--- GADE6122DavidTristanPOE/EnemyTile.cs
+++ GADE6122DavidTristanPOE/EnemyTile.cs
@@ -1,21 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GADE6122DavidTristanPOE
+﻿namespace GADE6122DavidTristanPOE
 {
-    internal  abstract class EnemyTile : CharacterTile
+    internal abstract class EnemyTile : CharacterTile
     {
         public EnemyTile(Position position, int hitPoints, int attackPower) : base(position, hitPoints, attackPower)
         {
-            
         }
 
-       //(Q2.1) public abstract void GetMove(out Tile);
-        
-
-        public override char Display => throw new NotImplementedException();
+       public abstract bool GetMove(out Tile tile);
     }
 }
