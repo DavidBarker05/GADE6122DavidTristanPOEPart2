d037c933e5c95924a9a8f66f5a470665ff5befbf
Tristan-Robinson <ST10439581@vgconnect.edu.za>
2024-10-07 13:57:27 +0200

    EnemyTile

---
+++ GADE6122DavidTristanPOE/EnemyTile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6122DavidTristanPOE
+{
+    internal  abstract class EnemyTile : CharacterTile
+    {
+        public EnemyTile(Position position, int hitPoints, int attackPower) : base(position, hitPoints, attackPower)
+        {
+
+        }
+
+       //(Q2.1) public abstract void GetMove(out Tile);
+        
+
+        public override char Display => throw new NotImplementedException();
+    }
+}
