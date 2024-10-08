53d1a08dde492f90436cb8b31def65863ba1090f
DavidBarker05 <davidbarker9611@gmail.com>
2024-10-08 18:52:28 +0200

    Fixed formatting and removed unecessary usings

--- GADE6122DavidTristanPOE/GameEngine.cs
+++ GADE6122DavidTristanPOE/GameEngine.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace GADE6122DavidTristanPOE
 {
--- GADE6122DavidTristanPOE/Level.cs
+++ GADE6122DavidTristanPOE/Level.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Deployment.Application;
 
 namespace GADE6122DavidTristanPOE
 {
