48af3d0ff9c9138ffb176faaf86ee358e64e02a0
DavidBarker05 <davidbarker9611@gmail.com>
2024-10-08 18:57:21 +0200

    Update form look to include attacks

--- GADE6122DavidTristanPOE/Form1.Designer.cs
+++ GADE6122DavidTristanPOE/Form1.Designer.cs
@@ -65,11 +65,11 @@
             // lblObjective
             // 
             this.lblObjective.AutoSize = true;
-            this.lblObjective.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblObjective.Location = new System.Drawing.Point(28, 111);
+            this.lblObjective.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObjective.Location = new System.Drawing.Point(63, 111);
             this.lblObjective.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblObjective.Name = "lblObjective";
-            this.lblObjective.Size = new System.Drawing.Size(226, 198);
+            this.lblObjective.Size = new System.Drawing.Size(188, 162);
             this.lblObjective.TabIndex = 2;
             this.lblObjective.Text = "Objective:\r\nGet the Hero to\r\nthe Level Exit by\r\nmoving through the\r\nEmpty Spaces " +
     "and\r\ndefeating Enemies.\r\nUse Health Pickup\r\nto heal lost hit\r\npoints.";
@@ -81,26 +81,27 @@
             this.lblKey.Location = new System.Drawing.Point(469, 111);
             this.lblKey.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblKey.Name = "lblKey";
-            this.lblKey.Size = new System.Drawing.Size(238, 162);
+            this.lblKey.Size = new System.Drawing.Size(238, 144);
             this.lblKey.TabIndex = 3;
-            this.lblKey.Text = "Key:\r\n█ = Wall\r\n. = Empty Space\r\n▼ = Hero (Alive)\r\nx = Hero (Dead)\r\n▒ = Level Exi" +
-    "t\r\n+ = Health Pickup\r\nϪ = Enemy Grunt (Alive)\r\nx = Enemy Grunt (Dead)";
+            this.lblKey.Text = "Key:\r\n█ = Wall\r\n. = Empty Space\r\n▼ = Hero (Alive)\r\n▒ = Level Exit\r\n+ = Health Pic" +
+    "kup\r\nϪ = Enemy Grunt (Alive)\r\nx = Enemy Grunt (Dead)";
             // 
             // lblControls
             // 
             this.lblControls.AutoSize = true;
-            this.lblControls.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblControls.Location = new System.Drawing.Point(28, 363);
+            this.lblControls.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblControls.Location = new System.Drawing.Point(63, 311);
             this.lblControls.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblControls.Name = "lblControls";
-            this.lblControls.Size = new System.Drawing.Size(178, 110);
+            this.lblControls.Size = new System.Drawing.Size(168, 162);
             this.lblControls.TabIndex = 4;
-            this.lblControls.Text = "Controls:\r\nW = Move Up\r\nA = Move Left\r\nS = Move Down\r\nD = Move Right";
+            this.lblControls.Text = "Controls:\r\nW = Move Up\r\nA = Move Left\r\nS = Move Down\r\nD = Move Right\r\n↑ = Attack " +
+    "Up\r\n← = Attack Left\r\n↓ = Attack Down\r\n→ = Attack Right";
             // 
             // lblHitPoints
             // 
             this.lblHitPoints.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHitPoints.ForeColor = System.Drawing.Color.Brown;
+            this.lblHitPoints.ForeColor = System.Drawing.Color.Red;
             this.lblHitPoints.Location = new System.Drawing.Point(253, 473);
             this.lblHitPoints.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblHitPoints.Name = "lblHitPoints";
@@ -122,7 +123,7 @@
             this.Controls.Add(this.lblLevelNumber);
             this.Controls.Add(this.lblDisplay);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Dungeon Escape";
