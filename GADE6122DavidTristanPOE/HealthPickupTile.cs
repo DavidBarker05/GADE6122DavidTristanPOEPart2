bb5e99c1e207a07fea4fb848c95eb9918166ac55
DavidBarker05 <davidbarker9611@gmail.com>
2024-10-08 19:49:23 +0200

    Finished HealthPickupTile comments

--- GADE6122DavidTristanPOE/HealthPickupTile.cs
+++ GADE6122DavidTristanPOE/HealthPickupTile.cs
@@ -9,10 +9,11 @@
 
         public override char Display => '+'; // The character that represents a HealthPickupTile
 
+        // Implement the ApplyEffect method defined in the PickupTile class to heal a character
         public override void ApplyEffect(CharacterTile characterTile)
         {
-            characterTile.Heal(10);
-            if (characterTile.HitPoints > characterTile.MaxHitPoints) characterTile.HitPoints = characterTile.MaxHitPoints;
+            characterTile.Heal(10); // Heal 10 hit points
+            if (characterTile.HitPoints > characterTile.MaxHitPoints) characterTile.HitPoints = characterTile.MaxHitPoints; // Clamp the hit points to the max hit points
         }
     }
 }
